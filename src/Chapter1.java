594f5cfcba0996a0b396847b8821c40ce1baf413
kokikohara <kokikohara@localhost>
2021-05-01 14:44:14 +0900

    code1-1

---
+++ src/Chapter1.java
@@ -0,0 +1,11 @@
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		System.out.println("RPG:スッキリ魔王征伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("<ただいま鋭意学集中・製作中>");
+		System.out.println("プログラムを終了します");
+	}
+
+}

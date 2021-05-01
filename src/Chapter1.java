0c3ad1aa312804876000c2dd90ba93dcd653bef9
kouharakouki <koki.kohara916@gmail.com>
2021-05-01 15:39:08 +0900

    code1-3

--- src/Chapter1.java
+++ src/Chapter1.java
@@ -8,8 +8,10 @@ public class Chapter1 {
 		System.out.println("プログラムを終了します");
 
 		int age;
-		age = 30;
-		System.out.println(age);
+		age = 20;
+		System.out.println("私の年齢は" + age);
+		age = 31;
+		System.out.println("いや。本当の年齢は" + age);
 	}
 
 }

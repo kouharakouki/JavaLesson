272527e92bda7da56b22c023e6b15bdd213ce095
kouharakouki <koki.kohara916@gmail.com>
2021-05-02 20:33:59 +0900

    Fix Lesson2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -16,7 +16,7 @@ public class Lesson2 {
 		System.out.println("あなたの年齢を入力してください");
 		String ageString = new java.util.Scanner(System.in).nextLine();
 		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(3);
+		int fortune = new java.util.Random().nextInt(4);
 		fortune++;
 		System.out.println("占いの結果が出ました！");
 		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");

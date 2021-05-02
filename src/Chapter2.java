1efe2cc6eb59f813f316e4f485dc63cb5ac03f75
kouharakouki <koki.kohara916@gmail.com>
2021-05-02 14:44:12 +0900

    ~ code2-10

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -26,6 +26,23 @@ public class Chapter2 {
 		System.out.println(f);
 		System.out.println(d);
 
+//		強制的な型変換→よほどの理由がない限り使わない
+		int age = (int)3.2;
+		System.out.println(age);
+
+//		異なる型同士の算術演算
+		double aa = 8.5 / 2;
+		long bb = 5 + 2L;
+		System.out.println(aa);
+		System.out.println(bb);
+
+//		文字列の連結
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+
+		String name = "すがわら";
+		String message = name + "さん、こんにちは！";
+		System.out.println(message);
 
 	}
 

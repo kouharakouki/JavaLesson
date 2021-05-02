9561ea1841df55ca1f75eff6c2d0e64a360debfd
kouharakouki <koki.kohara916@gmail.com>
2021-05-02 16:21:10 +0900

    Finish Chapter2

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -40,9 +40,38 @@ public class Chapter2 {
 		String msg = "私の年齢は" + 23;
 		System.out.println(msg);
 
+
+//		＜命令実行の文＞
+//		画面に表示する命令文
 		String name = "すがわら";
 		String message = name + "さん、こんにちは！";
 		System.out.println(message);
+
+//		改行なし画面出力の命令
+		System.out.print("私の名前は");
+		System.out.println(name);
+
+//		大きい方の数値を代入する命令
+		int h = 5;
+		int i = 3;
+		int l = Math.max(h, i);
+		System.out.println("比較実験:" + h + "と" + i + "とで大きい方は・・・" + l);
+
+//		文字列を数値に変換する命令
+		String age1 = "31";
+		int n = Integer.parseInt(age1);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね");
+
+//		乱数を生み出して代入する命令文
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたは多分、" + r + "歳ですかね？");
+
+//		キーボードから入力を受け付ける文
+		System.out.println("あなたの名前を入力してください");
+		String Name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください");
+		int Age = new java.util.Scanner(System.in).nextInt();
+		System.out.println("ようこそ、" + Age + "歳の"  + Name + "さん");
 
 	}
 

cbe76cee51360608c8b4efa5eba573ebf0e46879
kouharakouki <koki.kohara916@gmail.com>
2021-05-08 13:32:53 +0900

    Finish Chapter6 Code ~6-4

---
+++ src/Calc.java
@@ -0,0 +1,12 @@
+
+public class Calc {
+
+//	ソースファイルを分割する
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println("足すと" + total + "引くと" + delta);
+	}
+
+}

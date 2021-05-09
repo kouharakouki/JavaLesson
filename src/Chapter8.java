73541bada487d6b6e16d10ecc3bf23ef6d1511e7
kouharakouki <koki.kohara916@gmail.com>
2021-05-09 18:55:30 +0900

    Finish Chapter8

--- src/Chapter8.java
+++ src/Chapter8.java
@@ -2,7 +2,25 @@
 public class Chapter8 {
 
 	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
 
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+
+		h.sit(5);
+		m1.run();
+		m2.run();
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 
 }

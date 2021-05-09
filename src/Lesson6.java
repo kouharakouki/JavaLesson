5202429227b03e60eda90bb79a697246b24d878a
kouharakouki <koki.kohara916@gmail.com>
2021-05-09 13:40:51 +0900

    Finish Lesson6

---
+++ src/Lesson6.java
@@ -0,0 +1,12 @@
+import comment.Zenhan;
+
+public class Lesson6 {
+
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.calldeae();
+		comment.Kouhan.showMondokoro();
+	}
+
+}

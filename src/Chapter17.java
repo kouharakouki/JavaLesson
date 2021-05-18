2a9cbac29ca7b47f86fd009d0937e0c19ab6b69d
kouharakouki <koki.kohara916@gmail.com>
2021-05-18 10:36:45 +0900

    Finish Chapter17

--- src/Chapter17.java
+++ src/Chapter17.java
@@ -1,18 +1,41 @@
 import java.io.FileWriter;
+import java.io.IOException;
 
 //例外処理を学ぶ
 public class Chapter17 {
 
 	public static void main(String[] args) {
+		FileWriter fw = null;
 		try {
-			FileWriter fw = new FileWriter("data.txt");
+			fw = new FileWriter("data.txt");
 			fw.write("hello!");
-			fw.close();
 		} catch(Exception e) {
 			System.out.println("何らかのエラーが発生しました");
 		} finally {
-
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch(IOException e) {
+					;
+				}
+			}
 		}
+
+//		上記の文を簡潔にまとめることもできる
+		try (FileWriter file = new FileWriter("data.txt");) {
+			file.write("hello!");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		}
+
+
+		try {
+			throw new UnsupportedMusicFileException
+				("未対応のファイルです");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ src/UnsupportedMusicFileException.java
@@ -0,0 +1,9 @@
+//オリジナルの例外クラスを定義する
+
+public class UnsupportedMusicFileException extends Exception {
+
+	public UnsupportedMusicFileException(String msg) {
+		super(msg);
+	}
+
+}
